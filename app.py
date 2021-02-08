b970a7b60274c31453772e637a7aa1a08aaa111a
Sai <Sai-yLord@users.noreply.github.com>
2021-02-08 21:49:20 +0600

    added book edditing

--- app.py
+++ app.py
@@ -16,32 +16,13 @@ def books():
     object_list = [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
     return render_template("books.htm", object_list=object_list)
 
-
-    # tales = [tale.value for tale in page["A"]][1:]
-    # tales = []
-    # for tale in page["A"][1:]:
-    #     tales.append(tale.value)
-
-    # authors = [author.value for author in page["B"]][1:]   
-
-    # html = """
-    #     <a href="/authors">Авторы</a>
-    #     <a href="/books">Книги</a>
-    #     <h1 style="color: red">Тут будет список книг:</h1>
-    # """
-
-    # for i in range(len(tales)):
-    #     html += f"<h2>{tales[i]} - {authors[i]}</h2>"
-
-    return html
-
-
 @app.route("/authors/")
 def authors():
     excel = load_workbook("tales1.xlsx")
     page = excel["Лист1"]
     authors = {author.value for author in page["B"][1:]}
     return render_template("authors.html", authors=authors)
+    
 
 @app.route("/add/", methods=["POST"])
 def add():
@@ -51,15 +32,38 @@ def add():
     last = len(page["A"]) + 1
     page[f"A{last}"] = f["book"]
     page[f"B{last}"] = f["author"]
+    page[f"C{last}"] = f["photo"]
     excel.save("tales1.xlsx")
-    return "форма получена"
+    return "Форма Получена!"
 
-@app.route("/book<num>")
+@app.route("/book/<num>/")
 def book(num):
     excel = load_workbook("tales1.xlsx")
     page = excel["Лист1"]
     object_list = [[tale.value, tale.offset(column=1).value, tale.offset(column=2).value] for tale in page["A"][1:]]
     obj = object_list[int(num)]
-    link_list = [[tale.value, tale.offset(column=1).value, tale.offset(column=2).value] for tale in page["D"][1:]]
-    link = link_list[int(num)]
+    obj.append(num)
     return render_template("book.htm", obj=obj)
+
+@app.route("/book/<num>/edit/")
+def book_edit(num):
+    num = int(num) + 2
+    excel_file = load_workbook("tales1.xlsx")
+    page = excel_file["Лист1"]
+    tale = page[f"A{num}"]
+    author = page[f"B{num}"]
+    image = page[f"C{num}"]
+    obj = [tale.value, author.value, image.value, num]
+    return render_template("book_edit.html", obj=obj)
+
+@app.route("/book/<num>/save/", methods=["POST"])
+def book_save(num):
+    num = int(num)
+    excel_file = load_workbook("tales1.xlsx")
+    page = excel_file["Лист1"]
+    form = request.form
+    page[f"A{num}"] = form["tale"]
+    page[f"B{num}"] = form["author"]
+    page[f"C{num}"] = form["image"]
+    excel_file.save("tales1.xlsx")
+    return "Сохранено!"
